302a6ff9249e685f829abb9b1743c09f2b998afe
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-01-05 22:13:14 -0300

    Tema_3-Queries

--- Tema_3-Queries.sql
+++ Tema_3-Queries.sql
@@ -18,4 +18,4 @@ SELECT * FROM profesor LIMIT 5;
 SELECT * FROM profesor WHERE apellido LIKE 'P%';
 
 -- Los profesores que nacieron en la década del 80 y tienen un salario mayor a 80000
-SELECT * FROM profesor WHERE fecha_nacimiento LIKE '198_-__-__' and salario > 80000;
+SELECT * FROM profesor WHERE fecha_nacimiento BETWEEN '1980-01-01' AND '1989-12-31' AND salario > 80000;
